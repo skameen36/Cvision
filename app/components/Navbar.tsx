f314481adadc6a134a6ba0eb6b781dc13472187d
shaikh ameen shaikh abdullah <imameen36@gmail.com>
2025-08-20 23:55:59 +0530

    created the first landing Ui

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <Link to="/"><p className='text-2xl font-bold text-gradient'>CVision</p></Link>
+        <Link to="/upload" className='primary-button w-fit'><p className='text-2xl font-bold text-gradient'>CVision</p></Link>
+    </nav>
+  )
+}
+
+export default Navbar
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import  Navbar  from "../components/Navbar";
+import type { Key } from "react";
+import { resumes } from "../../constants/index";
+import ResumeCard from "~/components/ResumeCard";
+
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Cvision" },
+    { name: "description", content: "clear AI-powered resume insight!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <Navbar />
+            <section className="main-section"> 
+              <div className="page-heading">
+                <h1>Track Your Applications & Resume Ratings</h1>
+                <h2>Review your submissions and check AI-powered feedback.</h2>
+              </div>
+
+
+              {resumes.length > 0 && (
+                <div className="resumes-section">
+                      {resumes.map((resume : Resume) => (
+                        <ResumeCard key={resume.id} resume={resume}/>
+                      ))}
+                </div>
+              )}
+
+            </section>
+         </main>;
 }
